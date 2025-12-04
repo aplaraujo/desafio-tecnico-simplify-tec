bbd7abe31d9c7d6804a6f1b9beda49c1d15f16b6
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-04 15:18:19 -0300

    Entities created and database configuration

---
+++ src/main/java/io/github/aplaraujo/entities/Todo.java
@@ -0,0 +1,71 @@
+package io.github.aplaraujo.entities;
+
+import io.github.aplaraujo.entities.enums.PriorityType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean done;
+
+    @Enumerated(EnumType.STRING)
+    private PriorityType priority;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public PriorityType getPriority() {
+        return priority;
+    }
+
+    public void setPriority(PriorityType priority) {
+        this.priority = priority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/io/github/aplaraujo/entities/enums/PriorityType.java
@@ -0,0 +1,5 @@
+package io.github.aplaraujo.entities.enums;
+
+public enum PriorityType {
+    ALTA, MEDIA, BAIXA
+}
