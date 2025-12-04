3a14712ebc4c10106dc26648650214c56fbfc748
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-04 15:25:32 -0300

    Services and controllers created

---
+++ src/main/java/io/github/aplaraujo/controllers/TodoController.java
@@ -0,0 +1,9 @@
+package io.github.aplaraujo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/todos")
+public class TodoController {
+}
