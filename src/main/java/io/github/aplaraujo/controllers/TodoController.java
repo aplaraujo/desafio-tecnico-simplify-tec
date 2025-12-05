bc055e5b8c8a2be2a6a0145b95a27718411bb64d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-04 22:03:20 -0300

    Find todo by id

--- src/main/java/io/github/aplaraujo/controllers/TodoController.java
+++ src/main/java/io/github/aplaraujo/controllers/TodoController.java
@@ -2,25 +2,35 @@ package io.github.aplaraujo.controllers;
 
 import io.github.aplaraujo.dto.TodoRequestDTO;
 import io.github.aplaraujo.dto.TodoResponseDTO;
+import io.github.aplaraujo.mappers.TodoMapper;
 import io.github.aplaraujo.services.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/todos")
 public class TodoController {
     private final TodoService todoService;
+    private final TodoMapper todoMapper;
 
-    public TodoController(TodoService todoService) {
+    public TodoController(TodoService todoService, TodoMapper todoMapper) {
         this.todoService = todoService;
+        this.todoMapper = todoMapper;
     }
 
     @PostMapping
     public ResponseEntity<TodoResponseDTO> save(@RequestBody TodoRequestDTO request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(todoService.save(request));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TodoResponseDTO> findById(@PathVariable("id") String id) {
+        var todoId = Long.parseLong(id);
+
+        return todoService.findById(todoId).map(todo -> {
+            TodoResponseDTO dto = todoMapper.toResponse(todo);
+            return ResponseEntity.ok(dto);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/io/github/aplaraujo/services/TodoService.java
+++ src/main/java/io/github/aplaraujo/services/TodoService.java
@@ -3,9 +3,12 @@ package io.github.aplaraujo.services;
 import io.github.aplaraujo.dto.TodoRequestDTO;
 import io.github.aplaraujo.dto.TodoResponseDTO;
 import io.github.aplaraujo.entities.Todo;
+import io.github.aplaraujo.exceptions.ResourceNotFoundException;
 import io.github.aplaraujo.mappers.TodoMapper;
 import io.github.aplaraujo.repositories.TodoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -21,5 +24,12 @@ public class TodoService {
         Todo todo = todoMapper.toEntity(request);
         todoRepository.save(todo);
         return todoMapper.toResponse(todo);
+    }
+
+    public Optional<Todo> findById(Long id) {
+        if (!todoRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Todo not found!");
+        }
+        return todoRepository.findById(id);
     }
 }
