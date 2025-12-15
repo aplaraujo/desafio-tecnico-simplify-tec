037a8a631c686287d2aa977289b3af5d83b31847
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-15 07:49:37 -0300

    SQL script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_role(name) VALUES ('USER');
+
+INSERT INTO tb_user(login, password, name) VALUES ('ritinha', '$2a$16$wbJbFL1fR2fgz88ANOAm3e9MLozBvimPBDOYEKrTePZIIMT7L7KBy', 'Ritinha');
+INSERT INTO tb_user(login, password, name) VALUES ('zequinha', '$2a$16$wbJbFL1fR2fgz88ANOAm3e9MLozBvimPBDOYEKrTePZIIMT7L7KBy', 'Zequinha');
